c92ed461f8ea55b05853cfdd76bf7e334e565843
edneiCristian <edneicristian@gmail.com>
2018-09-24 20:47:21 -0300

    Acrescentada a pergunta como esta

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -5,6 +5,7 @@ namespace ProjetoTeste {
         static void Main(string[] args) {
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
             Console.Read();
         }
     }
