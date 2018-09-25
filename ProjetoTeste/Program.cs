f46d089272e4f84a28d14aef6883dc47977e3ebd
edneiCristian <edneicristian@gmail.com>
2018-09-24 21:18:35 -0300

    atualizado

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -6,6 +6,7 @@ namespace ProjetoTeste {
             Console.WriteLine("Olá Mundo!");            
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("Tchau!");
             Console.Read();
         }
     }
